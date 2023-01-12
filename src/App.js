ad6d5152002642d7c1514fae823b119fe4f3f356
Alex Via <alexandervia@gmail.com>
2023-01-12 11:01:22 -0600

    access api with useEffect and fetch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import axios from 'axios'
+// import axios from 'axios'
+import React from 'react';
 import './App.css';
 
 function App() {
-  function getData() {
-    axios.get("http://localhost:3001/products").then(response => {
-      console.log(response.data)
-    })
-    // console.log("clicked")
-  }
+  // function getData() {
+  //   axios.get("http://localhost:3001/products").then(response => {
+  //     console.log(response.data)
+  //   })
+  //   // console.log("clicked")
+  // }
+
+  const [products, setProducts] = React.useState([])
+
+  React.useEffect(() => {
+    fetch("http://localhost:3001/products")
+    .then(res => res.json())
+    .then(data => setProducts(data))
+  }, [])
+  console.log(products)
+
+  const productDisplay = products.map(product => {
+    return(
+      <h1 key={product.id}>{product.name}</h1>
+    )
+  })
   return (
     <div className="App">
-      <button onClick={getData}>Click me</button>
+      {productDisplay}
     </div>
   );
 }
