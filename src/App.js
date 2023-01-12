3e94dd89fddb6d453c335b5e0bfbf48c8afe7aea
Alex Via <alexandervia@gmail.com>
2023-01-12 11:21:26 -0600

    adds product image and price

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-// import axios from 'axios'
 import React from 'react';
 import './App.css';
 
 function App() {
-  // function getData() {
-  //   axios.get("http://localhost:3001/products").then(response => {
-  //     console.log(response.data)
-  //   })
-  //   // console.log("clicked")
-  // }
+    const [products, setProducts] = React.useState([])
 
-  const [products, setProducts] = React.useState([])
+    React.useEffect(() => {
+        fetch("http://localhost:3001/products")
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    }, []);
+    console.log(products);
 
-  React.useEffect(() => {
-    fetch("http://localhost:3001/products")
-    .then(res => res.json())
-    .then(data => setProducts(data))
-  }, [])
-  console.log(products)
-
-  const productDisplay = products.map(product => {
-    return(
-      <h1 key={product.id}>{product.name}</h1>
-    )
-  })
-  return (
-    <div className="App">
-      {productDisplay}
-    </div>
-  );
+    const productDisplay = products.map(product => {
+        return(
+            <div key={product.id}>
+                <h1 >{product.name}</h1>
+                <img src={product.image_url} alt=''/>
+                <h4>Price: ${product.price}</h4>
+                <hr/>
+            </div>
+        )
+    });
+    return (
+        <div className="App">
+            {productDisplay}
+        </div>
+    );
 }
 
 export default App;
